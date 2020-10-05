8a001e5979a541948984ece0755c14f99c68139e
Mayur Chhapra <30775975+mayurchhapra@users.noreply.github.com>
2020-10-06 01:39:56 +0530

    Revert "Added db Config"

--- src/config/db.js
+++ src/config/db.js
@@ -1,32 +1,5 @@
-const mongoose = require('mongoose');
-const uriFormat = require('mongodb-uri');
+const {MongoClient} = require('mongodb');
 
-const configDb = {
-  host: process.env.MONGODB_URL,
-  server: {
-    socketOptions: {
-      keepAlive: 1,
-      connectTimeoutMS: 15000,
-    },
-  },
-};
-
-function encodeMongoURI(urlStringParam) {
-  let urlString;
-  if (urlStringParam) {
-    const parsed = uriFormat.parse(urlStringParam);
-    urlString = uriFormat.format(parsed);
-  }
-  return urlString;
-}
-
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-mongoose.connect(encodeMongoURI(configDb.host));
-
-const db = mongoose.connection;
+const db = new MongoClient(process.env.MONGODB_URL);
 
 module.exports = db;
